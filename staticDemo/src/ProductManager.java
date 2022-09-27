e4aef2af2c4e1fdf5bd8932d4a40d7ed7afceea2
MehmetUySaL07 <83504169+MehmetUySaL07@users.noreply.github.com>
2022-09-27 23:17:28 +0300

    Update ProductManager.java

--- staticDemo/src/ProductManager.java
+++ staticDemo/src/ProductManager.java
@@ -1,14 +1,6 @@
 public class ProductManager {
     public void add(Product product) {
-        //first way-without static
-        /*ProductValidator validator = new ProductValidator();
-        if (validator.isValid(product)) {
-            System.out.println("Product is added");
-        }else {
-            System.out.println("Product info is not valid");
-        }*/
-
-        //second way ProductValidator should be static at the same time
+      
         if (ProductValidator.isValid(product)) {
             System.out.println("Product is added");
         }else {
